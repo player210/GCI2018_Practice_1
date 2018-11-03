210f232542efe5ed8dbf0e112cb1e79c07a73f8b
player210 <32802659+player210@users.noreply.github.com>
2018-11-03 12:41:15 -0700

    Add files via upload

---
+++ GCI python.py
@@ -0,0 +1,7 @@
+x=10
+for i in range (x):
+    print("GCI is great")
+name = input("What is your name?")
+print("Hello " + name)
+name = name[::-1]
+print("Did you know that your name is "+name+" backwards")
